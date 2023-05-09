e79ab71c814a8d6d6f2caa024720dc0cedc3a69a
Ian Gordon <ianbgordon@gmail.com>
2023-05-09 13:34:22 -0400

    converted clock object into class

--- components/clock.js
+++ components/clock.js
@@ -1,31 +1,37 @@
-let clock = {
-    state: 0, // the state can either be 0 or 1
-    running: false,
-    speed: 1, // this is measured in cycles per second
-    advance: (n = 1) => {
+export class Clock {
+    constructor() {
+        this.state = 0; // the state can either be 0 or 1
+        this.running = false;
+        this.speed = 1.0; // this is measured in cycles per second
+        this.up_hook = () => {
+            console.log(1); // this is by default but should be replaced
+        };
+        this.down_hook = () => {
+            console.log(0); // this is by default but should be replaced
+        },
+    }
+
+    advance(n = 1) {
         for (let i = 0; i < n; i++) {
-            clock.state = (clock.state == 0) ? 1 : 0;
-            if (clock.state == 1) {
-                clock.up_hook();
+            this.state = (this.state == 0) ? 1 : 0;
+            if (this.state == 1) {
+                this.up_hook();
             } else {
-                clock.down_hook();
+                this.down_hook();
             }
         }
-    },
-    run: async () => {
-        clock.running = true;
-        while (clock.running) {
-            await new Promise(r => setTimeout(r, 500 / clock.speed));
-            clock.advance();
+    }
+
+    async run() {
+        this.running = true;
+        while (this.running) {
+            await new Promise(r => setTimeout(r, 500 / this.speed));
+            this.advance();
         }
-    },
-    stop: () => {
-        clock.running = false;
-    },
-    up_hook: () => {
-        console.log(1); // this is by default but should be replaced
-    },
-    down_hook: () => {
-        console.log(0); // this is by default but should be replaced
-    },
+    }
+
+    stop() {
+        this.running = false;
+    }
+
 };
