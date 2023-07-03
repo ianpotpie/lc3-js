5cd1fee0a7e8f4fb7d1aeb59041dc0d2bb23e167
Ian Gordon <ianbgordon@gmail.com>
2023-07-02 20:51:30 -0400

    refactor run to use setInterval

--- components/clock.js
+++ components/clock.js
@@ -1,37 +1,39 @@
-export class Clock {
+class Clock {
     constructor() {
         this.state = 0; // the state can either be 0 or 1
-        this.running = false;
+        this.interval = null;
         this.speed = 1.0; // this is measured in cycles per second
         this.up_hook = () => {
             console.log(1); // this is by default but should be replaced
         };
         this.down_hook = () => {
             console.log(0); // this is by default but should be replaced
-        },
+        };
     }
 
     advance(n = 1) {
         for (let i = 0; i < n; i++) {
-            this.state = (this.state == 0) ? 1 : 0;
-            if (this.state == 1) {
+            if (this.state == 0) {
+                this.state = 1;
                 this.up_hook();
             } else {
+                this.state = 0;
                 this.down_hook();
             }
         }
     }
 
-    async run() {
-        this.running = true;
-        while (this.running) {
-            await new Promise(r => setTimeout(r, 500 / this.speed));
-            this.advance();
+    run() {
+        if (this.interval != null) {
+            clearInterval(this.interval);
         }
+        this.interval = setInterval(() => this.advance(), 1000 / this.speed);
     }
 
     stop() {
-        this.running = false;
+        if (this.interval != null) {
+            clearInterval(this.interval);
+        }
     }
 
-};
+}
